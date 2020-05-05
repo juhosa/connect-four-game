fc6640ec4953a655d8eed5e7539a14261236b72e
Juho Salli <juho.salli@gmail.com>
2020-05-05 15:46:16 +0300

    Drops the ball to correct cell and toggles players

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let cells = [];
 const COLS = 7;
 const ROWS = 6;
 
+let player = "red";
+
 function get_column_values(c_index) {
   let tmp = [];
   for (let i = 0; i < ROWS; i++) {
@@ -10,27 +12,44 @@ function get_column_values(c_index) {
   return tmp;
 }
 
-function cell_clicked(index) {
-  let id = index.split("_")[1];
-  console.log(`cell with id ${index} clicked. (Column ${id})`);
-
-  let col_vals = get_column_values(parseInt(id));
-  console.log(col_vals);
-
+function find_last_empty_row(col_vals) {
   // find index of the last empty row
-  last_empty = ROWS - 1;
+  let last_empty = ROWS - 1;
   for (let i = 0; i < col_vals.length; i++) {
     if (col_vals[i] !== "empty") {
       last_empty = i - 1;
       break;
     }
   }
-  console.log(last_empty);
-
-  document.querySelector(`#${index}`).dataset.color = "red";
+  return last_empty;
 }
 
-function toggle_cell_color(r, c) {}
+function cell_clicked(index) {
+  let id = index.split("_")[1];
+  console.log(`cell with id ${index} clicked. (Column ${id})`);
+
+  let col_vals = get_column_values(parseInt(id));
+  //   console.log(col_vals);
+
+  let last_empty_row_index = find_last_empty_row(col_vals);
+  //   console.log(last_empty_row_index);
+
+  toggle_cell_color(last_empty_row_index, parseInt(id));
+
+  // change player
+  player = player === "red" ? "blue" : "red";
+}
+
+function toggle_cell_color(r, c) {
+  // change the element dataset
+  let index = r * ROWS + r + c;
+  //   console.log(`r: ${r}, c: ${c}, index: ${index}`);
+
+  document.querySelector(`#cell_${index}`).dataset.color = player;
+
+  // change the value in cells-array
+  cells[index] = player;
+}
 
 function create_board() {
   let board = document.querySelector("#board");
