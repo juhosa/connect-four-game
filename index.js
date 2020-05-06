eaddb771080c59ce241200be084449f0e2a8ca6b
Juho Salli <juho.salli@gmail.com>
2020-05-06 08:26:47 +0300

    Check for if board is full. Update the UI to 3 columns. Add player player to the UI

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function find_last_empty_row(col_vals) {
 
 function cell_clicked(index) {
   let id = index.split("_")[1];
-  console.log(`cell with id ${index} clicked. (Column ${id})`);
+  //   console.log(`cell with id ${index} clicked. (Column ${id})`);
 
   let col_vals = get_column_values(parseInt(id));
   //   console.log(col_vals);
@@ -34,10 +34,55 @@ function cell_clicked(index) {
   let last_empty_row_index = find_last_empty_row(col_vals);
   //   console.log(last_empty_row_index);
 
+  if (last_empty_row_index === -1) {
+    return;
+  }
+
   toggle_cell_color(last_empty_row_index, parseInt(id));
 
   // change player
   player = player === "red" ? "blue" : "red";
+
+  // check board for possible win
+  let is_over = check_board();
+  console.log(`Is the game over: ${is_over}`);
+
+  updateUI();
+}
+
+function updateUI() {
+  document.querySelector("#player_span").innerText = player;
+}
+
+function check_board() {
+  console.log("checking the board...");
+  console.time("checking board");
+
+  // 0 = unchecked
+  // 1 = board full
+  // 2 = red won
+  // 3 = blue won
+  let board_state = 0;
+  let empty_found = false;
+  // loop cells
+  for (let i = 0; i < cells.length; i++) {
+    // if empty, pass
+    if (cells[i] === "empty") {
+      empty_found = true;
+      continue;
+    }
+
+    let val = cells[i];
+    let c_index = Math.floor(i % COLS);
+    let r_index = Math.floor(i / COLS);
+    // console.log(`index: ${i}, r: ${r_index}, c: ${c_index}, val: ${val}`);
+  }
+
+  console.timeEnd("checking board");
+  if (!empty_found) {
+    board_state = 1;
+  }
+  return board_state;
 }
 
 function toggle_cell_color(r, c) {
@@ -86,6 +131,7 @@ function init() {
   //   cells[28] = "red";
   //   cells[35] = "red";
   create_board();
+  updateUI();
 }
 
 init();
